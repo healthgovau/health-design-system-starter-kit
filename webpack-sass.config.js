7d5f8cae291bc4e77642d640db07393f2fb6e721
Hugo <hugo.heuman@health.gov.au>
2021-12-17 08:59:27 +0800

    Compiles and writes scss

---
+++ webpack-sass.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: [
+        './src/scss/main.scss',
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 'auto',
+        open: true,
+        historyApiFallback: true,
+        hot: true,
+        liveReload: true,
+        devMiddleware: {
+            writeToDisk: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /.(scss|sass)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist','css')
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].[chunkhash].js'
+        })
+    ]
+};
