1abe315e56e8176e37a49372ca986911635391d0
Danni <danni.marlow@outlook.com>
2023-10-03 09:39:37 +1100

    Site-level scripts
    - Filter control
    - 'view more' control

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,33 @@
-$( document ).ready(function() {
-    console.log( "HDS Starter Kit!!!" );
-});
+(function ($) {
+
+    $(document).ready(function () {
+        // Filter trial search toggles
+        $('.clinical-trials-filter__control').click(function (e) {
+            $('.clinical-trials-filter__form').toggleClass('clinical-trials-filter__form--mobile-hidden');
+        });
+
+        // Expand truncated field
+        $('.health-field--truncate__control').click(function (e) {
+            e.preventDefault();
+            var $target = $(this).prev();
+
+            if ($target.hasClass('health-field--truncate--closed')) {
+                $target.removeClass('health-field--truncate--closed')
+                $(this)
+                    .removeClass('health-field--truncate__control--closed')
+                    .attr('aria-expanded', 'true')
+                    .html('view less');
+            } else {
+                $target.addClass('health-field--truncate--closed');
+                $(this)
+                    .addClass('health-field--truncate__control--closed')
+                    .attr('aria-expanded', 'false')
+                    .html('view more');
+            }
+        });
+
+    });
+
+})(jQuery);
+
+console.log("Script loaded");
